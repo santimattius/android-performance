84111597bc05d901215b0bea67cd250297c93719
Santiago Mattiauda <santimattius@gmail.com>
2022-03-15 21:36:24 -0300

    [feature] macro-benchmark

--- macrobenchmark/src/main/java/com/santimattius/macrobenchmark/StartupBenchmark.kt
+++ macrobenchmark/src/main/java/com/santimattius/macrobenchmark/StartupBenchmark.kt
@@ -22,7 +22,7 @@ import org.junit.runner.RunWith
  */
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class ExampleStartupBenchmark {
+class StartupBenchmark {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
